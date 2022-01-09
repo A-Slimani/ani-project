e55ebdb3548efd9ae6292825c6bdbf948804ee15
A-Slimani <abdullahslimani98@gmail.com>
2022-01-09 23:12:05 +1100

    working on file detection and traversal

--- example_search_file.cpp
+++ example_search_file.cpp
@@ -5,21 +5,51 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-	DIR* dir = opendir("C:\\Users");
+/*
+	NOTES / TODO
+	- Figure out how to crawl through each directory (somewhat done)
+	- Figure out how to detect file types (done I think)
+	- 
+*/
+
+void listFiles(const char* dirname){
+	DIR* dir = opendir(dirname);
 
 	if (dir == NULL){
-		cout << "empty." << endl;
+		printf("empty.\n");
 	}
+
+	printf("----------------- \n");
+	printf("Reading files in: %s\n", dirname);
+	printf("----------------- \n");
 
 	struct dirent* entity;
 	entity = readdir(dir);
 
 	while(entity != NULL){
-		cout << entity -> d_name << endl;
+
+		// ignores dot files 
+		bool ignore_matrix = strcmp(entity->d_name, ".") != 0 && strcmp(entity->d_name, "..") != 0;
+
+		// prints files that are not dot files	
+		if (ignore_matrix) printf("%d %s\n", entity->d_type ,entity->d_name);
+
+		if(entity->d_type == DT_DIR && ignore_matrix){
+			char path[100] = { 0 };
+
+			strcat(path, dirname);
+			strcat(path, "/");
+			strcat(path, entity->d_name);
+
+			listFiles(path);
+		}
+
+		entity = readdir(dir);
 	}
-
 	closedir(dir);
+}
 
+int main(int argc, char* argv[]){
+	listFiles("/home/aboud/anime/");
 	return 0;
 }
